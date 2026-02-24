0c1cef99bac6bd69d1c25651222069cfb0496ff2
vinicius-figueira <viniciusfigueiraboaviagem@gmail.com>
2026-02-24 18:54:01 -0300

    Questão 1 - Herança

--- CPP 2/Figuras Geométricas - Herança/FiguraGeometrica.h
+++ CPP 2/Figuras Geométricas - Herança/FiguraGeometrica.h
@@ -1,65 +1,102 @@
-#pragma once
 #include <iostream>
 #include <string>
 using namespace std;
 
-class Figura {
-    protected:
-    string nome;
-    Figura(int i) {
-        
-           }
+class FiguraGeometrica {
+protected:
+    int tipo;
+
+public:
+    FiguraGeometrica(int tipo) {
+        this->tipo = tipo;
     }
-    float CalcularArea();
-    void LerAtributosArea();
-    string getNome(){
-        return nome;
-    };
+
+    virtual float calcularArea() = 0;
+    virtual void lerAtributosArea() = 0;
+    virtual string getNome() = 0;
+
+    virtual ~FiguraGeometrica() {}
 };
 
-class Quadrado : public Figura {
-    private:
+
+class Quadrado : public FiguraGeometrica {
+private:
     float lado;
-    public:
-    float CalcularArea(){
-        return lado*lado; 
+
+public:
+    Quadrado() : FiguraGeometrica(1) {}
+
+    void lerAtributosArea() override {
+        cin >> lado;
     }
-    void LerAtributosArea(){
-        cout << CalcularArea() << endl;
-    };
+
+    float calcularArea() override {
+        return lado * lado;
+    }
+
+    string getNome() override {
+        return "Quadrado";
+    }
 };
 
-class Retangulo : public Figura {
-    private:
-    float largura, altura;
-    public:
-    float CalcularArea(){
-        return largura*altura; 
-    }
-    void LerAtributosArea(){
-        cout << CalcularArea << endl;
-    };
-};
-class Triangulo : public Figura {
-    private:
+class Retangulo : public FiguraGeometrica {
+private:
     float base, altura;
-    public:
-    float CalcularArea(){
-        return (base*altura)/2; 
-    }
-    void LerAtributosArea(){
-        cout << CalcularArea() << endl;
-    };
-class Circulo : public Figura {
-    private: 
-    float raio;
-    public:
-    float CalcularArea(){
-        return (3,14 * raio * raio);
-    }
-    void LerAtributosArea(){
-        cout << CalcularArea() << endl;
-    }
-    };
 
+public:
+    Retangulo() : FiguraGeometrica(2) {}
+
+    void lerAtributosArea() override {
+        cin >> base >> altura;
+    }
+
+    float calcularArea() override {
+        return base * altura;
+    }
+
+    string getNome() override {
+        return "Retângulo";
+    }
+};
+
+
+class Triangulo : public FiguraGeometrica {
+private:
+    float base, altura;
+
+public:
+    Triangulo() : FiguraGeometrica(3) {}
+
+    void lerAtributosArea() override {
+        cin >> base >> altura;
+    }
+
+    float calcularArea() override {
+        return (base * altura) / 2.0;
+    }
+
+    string getNome() override {
+        return "Triângulo";
+    }
+};
+
+
+class Circulo : public FiguraGeometrica {
+private:
+    float raio;
+
+public:
+    Circulo() : FiguraGeometrica(4) {}
+
+    void lerAtributosArea() override {
+        cin >> raio;
+    }
+
+    float calcularArea() override {
+        return 3.14f * raio * raio;
+    }
+
+    string getNome() override {
+        return "Círculo";
+    }
 };
--- CPP 2/Figuras Geométricas - Herança/main.cpp
+++ CPP 2/Figuras Geométricas - Herança/main.cpp
@@ -1,8 +1,34 @@
 #include "FiguraGeometrica.h"
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-int main (){
+int main() {
+    int tipo;
+    cin >> tipo;
 
+    while (tipo != 0) {
+        FiguraGeometrica* figura;
+        switch (tipo) {
+            case 1: figura = new Quadrado(); break;
+            case 2: figura = new Retangulo(); break;
+            case 3: figura = new Triangulo(); break;
+            case 4: figura = new Circulo(); break;
+            default: cout << "Tipo de figura inválida" << endl; break;
+        }
+        if (figura != nullptr) {
+            figura->lerAtributosArea();
+            cout << figura->getNome() 
+                 << " de área " 
+                 << figura->calcularArea() 
+                 << endl;
+
+            delete figura;
+        }
+
+        cin >> tipo;
+    }
+
+    return 0;
 }
