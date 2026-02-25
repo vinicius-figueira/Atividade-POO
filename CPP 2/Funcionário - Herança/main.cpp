dd8a22bbba2a6d62c4d5a9d6504483bd47640d0b
vinicius-figueira <viniciusfigueiraboaviagem@gmail.com>
2026-02-25 18:23:54 -0300

    código funcional, adicionei o input e output

--- CPP 2/Funcionário - Herança/main.cpp
+++ CPP 2/Funcionário - Herança/main.cpp
@@ -10,6 +10,23 @@ int main() {
     string n;
     float s;
 
+    cin >> m;
+    cin.ignore();
+    getline(cin, n);
+    cin >> s;
+    f.setMatricula(m);
+    f.setNome(n);
+    f.setSalario(s);
+    cout << f.getMatricula() << " - " << f.getNome() << " - R$ " << f.getSalario() << endl;
+    cin >> m;
+    cin.ignore();
+    getline(cin, n);
+    cin >> s;
+    c.setMatricula(m);
+    c.setNome(n);
+    c.setSalario(s);
+    cout << c.getMatricula() << " - " << c.getNome() << " - R$ " << c.getSalario() << endl;
+
     
 
 }
